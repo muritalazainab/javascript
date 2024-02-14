4adbc0b33adbe6166847a4cd3c390b8092f94f48
muritalazainab <muritala21@icloud.com>
2024-02-14 09:48:02 +0100

    first commit

---
+++ operatorANDequality/script.js
@@ -0,0 +1,24 @@
+const a = 10;
+const b = 30;
+let result = 0;
+
+result = a + b;
+console.log(result);
+result = a - b;
+console.log(result);
+
+result = a * b;
+console.log(result);
+
+result = a / b;
+console.log(result);
+
+result = a ** b;
+console.log(result);
+
+result = a  % b;
+console.log(result);
+
+let number = 5
+number++
+console.log(number)
---
+++ variable/script.js
@@ -0,0 +1,99 @@
+// variables.
+
+// let
+// const
+
+let message = "Hello Eef";
+message = 'hello Zainab';
+console.log(message);
+
+ const greeting = "good morning my neighbour"
+ console.log(greeting);
+
+
+const _name$ = 'Al fulaaniy Ileya';
+console.log(_name$);
+console.log
+
+(message,greeting)
+
+// classwork
+
+
+let studentName = 'Muritala zainab';
+console.log(studentName);
+
+
+const _text$ = 'five minutes';
+console.log(_text$);
+
+// Data type
+// string - Number-BigInt-Boolean-sybol-null-undefined -typeof-NAN **object-array-date
+// string
+// single string -''
+// Double string -""
+// back ticks-''
+
+const singleQuote = 'who am I?'
+const doubleQuote = "who are you?"
+
+console.log(singleQuote);
+console.log(doubleQuote);
+
+let name = "Team Lead";
+const backTick = `hello ${name}`;
+console.log(backTick);
+ 
+let newName = `Toheeb`
+let favouriteDrink = `Bobo`;
+const details = `m`
+
+
+// classwork 2 
+
+const numberOne = 45
+const numberTwo = 10   
+
+const addition = numberOne + numberTwo;
+const subtraction = numberOne -  numberTwo
+const multiplication = numberOne * numberTwo
+const division = numberOne / numberTwo
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+
+const numberThree = '10';
+const numberFour = '14';
+console.log(numberThree + numberFour)
+
+
+// BOOLEAN - TRUE/FALSE
+
+
+const isbeautiful = true;
+if (isbeautiful) {
+    console.log("this woman is beautiful")
+    
+} else {
+    console.log("this woman is ugly")
+    
+}
+
+// null
+let age = null;
+console.log(age);
+
+// lundefined
+let x;
+console.log(x)
+
+// symbol
+const value1 = Symbol("hello")
+const value2 = Symbol("hello")
+console.log(value1.description)
+
+
+
+
