851b6ad9dfb345e2c682f9a1c98d9d35d2857f9d
muritalazainab <muritala21@icloud.com>
2024-02-27 10:43:42 +0100

    commit

--- objectANDMETHOD/script.js
+++ objectANDMETHOD/script.js
@@ -19,7 +19,7 @@ console.log(person)
 // ASSESSING AND ADDING OF PROPERTIES TO AN OBJECT 
 
 
-const human = {
+const human = { 
     firstName:'Abdullah',
     "DLT africa location": "pegamut"
 }
@@ -51,3 +51,62 @@ const dog = {
     }
 }
 dog.listAllProperties()
+// build is a properties that contain a function //
+
+const myObject = {
+    myMehod1: () => {}, // ARROW FUNCTION
+    myMehod2 : function(){}, // FUNCTION DECLEARATION
+   
+};
+const animal = {
+    name: "fluffy orange",
+    age: 10,
+    bark: () => {
+        console.log("woof woof!")
+    }
+}
+console.log(animal.name);
+animal.bark();
+
+// ************** object method
+// object.keys
+
+//  it creates and return an array containing the keys of an object
+
+const employees = {
+    Soliu: "boss",
+    Anate: "secretary",
+    Seyi: "Director",
+    Bode: "Sales",
+    Abdullah: "Assitant",
+
+}
+const employeeInfo = Object.keys(employees)
+console.log(employeeInfo)
+// object.values
+
+// it creates and rerurn an array contain the value of an object
+const section = {
+   id: 1,
+    Date: "27 feb 2024",
+    Device: "Mobile",
+    Browser: "Chrome",
+    
+}
+const values = Object.values(section)
+console.log(values)
+
+// object.entries
+// it creates a nested array of the key and values pairs of an oject
+
+const operatingSystem = {
+    name: "Linux",
+    version: 18.04,
+    license: "Open source"
+}
+const operatingSystemInfo = Object.entries(operatingSystem)
+console.log(operatingSystemInfo)
+
+
+
+
