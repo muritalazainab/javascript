1a85eab74d0dfbdb94ae14ff0350b6857e2bc221
muritalazainab <muritala21@icloud.com>
2024-03-13 10:25:52 +0100

    commit

--- random-quote-gen/script.js
+++ random-quote-gen/script.js
@@ -7,11 +7,26 @@ const authorName = document.getElementById("author")
 const  twitterButton = document.getElementById("twitter") ;
 
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader")
+
 
 // "https://type.fit/api/quotes"
 
-let apiQoute = [];
+let apiQoute = []
+
+// show loading
+function loading (){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+    
+}
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote() {
+    loading();
     const quote = apiQoute[Math.floor(Math.random() * apiQoute.length)];
     // console.log(quote);
     if(!quote.author){
@@ -29,11 +44,12 @@ function newQuote() {
 
     
     quoteText.textContent = quote.text
+    complete()
 }
 
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
-
+    loading();
     try {
         const response = await fetch(apiUrl);
          apiQoute = await response.json()
@@ -46,5 +62,16 @@ async function getQuotes() {
         console.log(error)  
     }
 }
+function tweetQuotes(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${ quoteText.textContent}-${authorName.textContent} `
+    window.open(twitterUrl, "_blank")
+
+}
+twitterButton.addEventListener("click", tweetQuotes)
+
+
+
+
 newQuoteBtn.addEventListener("click", newQuote)
 getQuotes()
+tweetQuotes()
